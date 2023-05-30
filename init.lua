32fe551f4c0f5b9fd9ec955b782ae57c91b29683
Michael Pollind <mpollind@gmail.com>
2023-05-29 21:37:44 -0700

    update floatterm

--- init.lua
+++ init.lua
@@ -394,6 +394,17 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
+  function FormatFunction()
+    vim.lsp.buf.format({
+      async = true,
+      range = {
+        ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
+        ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
+      }
+    })
+  end
+  vim.api.nvim_set_keymap("v", "\\f", "<Esc><cmd>lua FormatFunction()<CR>", {noremap = true})
+
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
--- lua/custom/plugins/floatterm.lua
+++ lua/custom/plugins/floatterm.lua
@@ -2,6 +2,6 @@ return {
     "numToStr/FTerm.nvim",
     version = "*",
     config = function()
-        vim.keymap.set("n", "<leader>t",require('FTerm').toggle, { desc = "[t]erminal" })
+        vim.keymap.set({"n", "t"}, "<C-t>",require('FTerm').toggle, { desc = "[t]erminal" })
     end,
 }
